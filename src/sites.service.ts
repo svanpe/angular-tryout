a46a8c70d8ae2d36aaca0868c26f52c8561ac1f8
svanpe <svanpe@gmail.com>
2023-04-24 18:14:31 +0200

    3 files modified

--- src/sites.service.ts
+++ src/sites.service.ts
@@ -11,7 +11,9 @@ export class SitesService {
   sites: Site[] = [ {id:1, status:'active', label: 'lion' }, {id: 2, status:'active', label: 'marché jolis bois'} ];
 
   timeslots: Timeslot[] = [
-    {start: new Date('2024-04-24 17:00:00'), end: new Date('2024-04-24 17:30:00'), bookingList:[], status:"free"}
+    {start: new Date('2024-04-24 15:00:00'), end: new Date('2024-04-24 16:00:00'), bookingList:[], status:"free"},
+    {start: new Date('2024-04-24 16:00:00'), end: new Date('2024-04-24 17:00:00'), bookingList:[{by: {firstName:'Grégoire', lastName:'Dupont'}},{by: {firstName:'Gilles', lastName:'Mentor'}}], status:"complete"},
+    {start: new Date('2024-04-24 17:00:00'), end: new Date('2024-04-24 18:00:00'), bookingList:[{by: {firstName:'Anthon', lastName:'Joubert'}}], status:"uncomplete"}
   ];
 
   getSites(){
